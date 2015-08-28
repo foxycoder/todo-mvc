98bb6050387cf91260710f2a8aae5b649e0991f4
Wouter de Vos <wouter@codaisseur.com>
2015-08-28 20:55:21 +0200

    Done.

---
+++ app/components/todo-list.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    actions: {
+        createTodo: function(newTitle) {
+            this.set('newTitle', '');
+            this.sendAction('createTodo', newTitle);
+        },
+
+        clearCompleted: function() {
+            var completed = this.get('model').filterBy('isCompleted', true);
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
+        }
+    },
+
+    remaining: Ember.computed('model.@each.isCompleted', function() {
+        var model = this.get('model');
+        return model.filterBy('isCompleted', false).get('length');
+    }),
+
+    inflection: Ember.computed('remaining', function() {
+        var remaining = this.get('remaining');
+        return (remaining === 1) ? 'item' : 'items';
+    }),
+
+    completed: Ember.computed('model.@each.isCompleted', function() {
+        var model = this.get('model');
+        return model.filterBy('isCompleted', true).get('length');
+    }),
+
+    hasCompleted: Ember.computed('completed', function() {
+         return this.get('completed') > 0;
+    }),
+
+    allAreDone: function(key, value) {
+         var model = this.get('model');
+         if (value === undefined) {
+             return model.get('lenght') > 0 && model.isEvery('isCompleted', true)
+         } else {
+             model.setEach('isCompleted', value);
+             model.invoke('save');
+             return value;
+         }
+    }.property('@each.isCompleted')
+});
